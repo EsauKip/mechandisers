da1641edfbbe12b01fd2358cc329469b1cd85e9f
vyonna <vyonna.njenga@student.moringaschool.com>
2022-04-29 11:18:42 +0300

    added views

--- Back-end/route/views.py
+++ Back-end/route/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_auth.registration.views import RegisterView
+from route.serializers import (
+    ManagerCustomRegistrationSerializer, MerchandiserCustomRegistrationSerializer
+    )
 
-# Create your views here.
+class ManagerRegistrationView(RegisterView):
+    serializer_class = ManagerCustomRegistrationSerializer
+
+
+class MerchandiserRegistrationView(RegisterView):
+    serializer_class = MerchandiserCustomRegistrationSerializer
